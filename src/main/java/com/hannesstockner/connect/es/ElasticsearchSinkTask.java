427f7dbbdbe157d6386f10037fbf7c1ad90ef6a9
chienlungcheung <chienlungcheung@gmail.com>
2019-11-07 21:14:09 +0800

    es 写入改为采用 BulkProcessor

--- src/main/java/com/hannesstockner/connect/es/ElasticsearchSinkTask.java
+++ src/main/java/com/hannesstockner/connect/es/ElasticsearchSinkTask.java
@@ -7,13 +7,14 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
-import org.elasticsearch.action.bulk.BulkItemResponse;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.bulk.*;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.slf4j.Logger;
@@ -23,7 +24,6 @@ import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.concurrent.*;
 
 public class ElasticsearchSinkTask extends SinkTask {
 
@@ -32,43 +32,7 @@ public class ElasticsearchSinkTask extends SinkTask {
   private String typeName;
   private String indexPrefix;
   private Client client;
-
-  private final ExecutorService writer = Executors.newSingleThreadExecutor();
-
-  private static class WriteTask implements Runnable {
-    private static BlockingDeque<BulkRequestBuilder> requests = new LinkedBlockingDeque<>(1000);
-    @Override
-    public void run() {
-      while (!Thread.currentThread().isInterrupted()) {
-        BulkRequestBuilder bulkRequestBuilder = null;
-        try {
-          bulkRequestBuilder = requests.take();
-        } catch (InterruptedException e) {
-          return;
-        }
-        int reqCnt = bulkRequestBuilder.numberOfActions();
-        BulkResponse bulkResponse = null;
-        try {
-          bulkResponse = bulkRequestBuilder.execute().actionGet(60, TimeUnit.SECONDS);
-          if (bulkResponse.hasFailures()) {
-            for (BulkItemResponse item : bulkResponse.getItems()) {
-              if (item.isFailed()) {
-                Monitor.getFailedRequests().incrementAndGet();
-              } else {
-                Monitor.getSuccessfulRequests().incrementAndGet();
-              }
-              log.warn("send to es failed: failure = {}", item.getFailure());
-            }
-          } else {
-            Monitor.getSuccessfulRequests().addAndGet(reqCnt);
-          }
-        } catch (Exception e) {
-          log.warn("send to es failed: exception = ", e);
-        }
-        Monitor.getToSentRequests().addAndGet(-reqCnt);
-      }
-    }
-  }
+  private BulkProcessor bulkProcessor;
   @Override
   public void start(Map<String, String> props) {
     AbstractConfig parsedConfig = new AbstractConfig(ElasticsearchSinkConnector.CONFIG_DEF, props);
@@ -94,24 +58,36 @@ public class ElasticsearchSinkTask extends SinkTask {
       }
       client = new PreBuiltTransportClient(settings)
         .addTransportAddresses(transportAddresses);
-
-      client
-        .admin()
-        .indices()
-        .preparePutTemplate("kafka_template")
-        .setPatterns(Collections.singletonList(indexPrefix + "*"))
-        .addMapping(typeName, new HashMap<String, Object>() {{
-          put("date_detection", false);
-          put("numeric_detection", false);
-        }})
-        .get();
-
     } catch (Exception e) {
       log.error("Couldn't connect to es: ", e);
       System.exit(-1);
     }
 
-    writer.submit(new WriteTask());
+    bulkProcessor = BulkProcessor.builder(client,
+      new BulkProcessor.Listener() {
+        @Override
+        public void beforeBulk(long executionId, BulkRequest request) {
+          Monitor.getToBeSentRequests().incrementAndGet();
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+          Monitor.getToBeSentRequests().decrementAndGet();
+          Monitor.getSuccessfulRequests().incrementAndGet();
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+          Monitor.getToBeSentRequests().decrementAndGet();
+          Monitor.getFailedRequests().incrementAndGet();
+        }
+      })
+      .setBulkActions(1000)
+      .setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB))
+      .setFlushInterval(TimeValue.timeValueSeconds(5))
+      .setConcurrentRequests(1)
+      .setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueMillis(100), 3))
+      .build();
   }
 
   @Override
@@ -121,7 +97,6 @@ public class ElasticsearchSinkTask extends SinkTask {
       return;
     }
     String indexSuffix = DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDateTime.now());
-    BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
     for (SinkRecord record : records) {
       log.debug("Processing record type = {}, record content = {}",
         record.value().getClass(),
@@ -129,24 +104,19 @@ public class ElasticsearchSinkTask extends SinkTask {
       IndexRequest indexRequest = client.prepareIndex(indexPrefix + record.topic() + "-" + indexSuffix, typeName)
         .setSource(gson.toJson(record), XContentType.JSON)
         .request();
-      bulkRequestBuilder.add(indexRequest);
-    }
-    try {
-      WriteTask.requests.offerFirst(bulkRequestBuilder, 500, TimeUnit.MICROSECONDS);
-      Monitor.getToSentRequests().addAndGet(bulkRequestBuilder.numberOfActions());
-    } catch (InterruptedException e) {
-      log.warn("queue of WriteTask is full, may be sending message to es is too slow. exception: ", e);
+      bulkProcessor.add(indexRequest);
     }
   }
 
   @Override
   public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {
+    bulkProcessor.flush();
   }
 
   @Override
   public void stop() {
     if (client != null) {
-      writer.shutdown();
+      bulkProcessor.close();
       client.close();
     }
   }
